d02735fec9a55fc92dc62800f4d25bc09491bbb0
Aniello Maiese <aniello.maiese@mecglobal.com>
2017-05-14 10:51:22 +0200

    DataFrame refactoring

--- main.py
+++ main.py
@@ -3,21 +3,25 @@ from bs4 import BeautifulSoup
 from tqdm import tqdm
 import csv
 import pandas as pd
+import sys
 
 IGNORED_HREF = ['#', 'javascript:;', 'javascript:void(0)']
 
 def detect_domain(url):
+    protocol = None
     if 'https' in url:
         domain = url.replace('https://','')
+        protocol = 'https'
     elif 'http' in url:
         domain = url.replace('http://','')
+        protocol = 'http'
     elif 'www' in url:
         domain = url.replace('www.','')
     if 'www' in url:
         domain = domain.replace('www.','')
     if domain.find('/')!= -1:
-        return domain[:domain.find('/')]
-    else: return domain
+        return domain[:domain.find('/')], protocol
+    else: return domain, protocol
 
 
 def get_soup(link):
@@ -28,12 +32,13 @@ def get_soup(link):
     soup = BeautifulSoup(request_object.content, 'html.parser')
     return soup
 
-def get_internal_link(url, base_url):
+
+def get_internal_link(url, domain):
     if url.has_attr('href'):
         url = url['href']
         if url not in IGNORED_HREF:
-            if base_url in url:
-                url = url.replace(base_url, '')
+            if domain in url:
+                url = url[url.find(domain):].replace(domain, '')
                 return url
             elif 'http' not in url:
                 if url:
@@ -45,77 +50,71 @@ def get_internal_link(url, base_url):
 class WPR(object):
     def __init__(self, url):
         self.nodes = []
-        self.connections = []
-        self.domain = detect_domain(url)
+        self.connections = pd.DataFrame(columns=['source','target'])
+        self.domain, self.protocol = detect_domain(url)
         self.visited_url = []
         self.graph = []
         self.visited_url = []
 
+    def get_internal_link(self, url):
+        if url.has_attr('href'):
+            url = url['href']
+            if url not in IGNORED_HREF:
+                if self.domain in url:
+                    url = url[url.find(self.domain):].replace(self.domain, '')
+                    return url
+                elif 'http' not in url:
+                    if url:
+                        if url[0] == '/':
+                            return url
+
     def get_links(self, url, protocol='https'):
-        if url not in self.visited_url:
-            self.base_url = protocol+'://' + self.domain + url
-            try:
-                soup = get_soup(self.base_url)
-                target_url = soup.findAll('a')
-                seed_targets = [get_internal_link(x, base_url) for x in target_url if get_internal_link(x, base_url)]
-                self.nodes += [url]
-                self.visited_url += [url]
+        if self.protocol: protocol = self.protocol
+        self.base_url = protocol+'://' + self.domain + url
+        try:
+            soup = get_soup(self.base_url)
+            target_url = soup.findAll('a')
+            seed_targets = [wpr.get_internal_link(x) for x in target_url if wpr.get_internal_link(x)]
+            return pd.DataFrame([{'source': url, 'target': x} for x in seed_targets])
 
-                for target in seed_targets:
-                    if target not in self.nodes:
-                        self.nodes.append(target)
-                print str(len(self.visited_url)) + '/' + str(len(self.nodes)) + ' Visited url'
-                return [{'source': url, 'target': x} for x in seed_targets]
-            except:
-                print base_url + '\n Not Found'
+        except:
+            print base_url + '\n Not Found'
+            return pd.DataFrame(columns=['source', 'target'])
 
 
-    def navigate_website(self, url, depth=2):
-        result = self.get_links(url)
-        if result: self.connections += result
+    def navigate_website(self, url, depth=2, protocol='https'):
+        result = self.get_links(url, protocol)
+        self.visited_url += [url]
+        self.connections = self.connections.append(result, ignore_index=True)
         counter = 1
         while counter <= depth:
-            print 'depth (' + str(counter) + '/' + str(depth) + ')'
-            next_urls = [x for x in self.connections if x['target'] not in self.visited_url]
-            for connection in next_urls:
-                base_url = connection['target']
-                result = self.get_links(base_url)
-                if result: self.connections += result
-            print 'Completed depth (' + str(counter) + '/' + str(depth) + ')'
+            next_urls = list(self.connections[~self.connections['target'].isin(self.visited_url)]['target'].unique())
+            #next_urls = list(set([x['target'] for x in self.connections if x['target'] not in self.visited_url]))
+            self.nodes = self.connections['target'].unique()
+            for url in next_urls:
+                result = self.get_links(url, protocol)
+                self.visited_url += [url]
+                self.connections = self.connections.append(result, ignore_index=True)
+                sys.stdout.write(str(len(self.visited_url)) + '/' + str(len(self.nodes)) + ' url '
+                                            '@ depth (' + str(counter) + '/' + str(depth) + ')\r')
+                sys.stdout.flush()
             counter += 1
-        next_urls = [x for x in self.connections if x['target'] not in self.visited_url]
-        for connection in next_urls:
-            base_url = connection['target']
-            result = self.get_links(base_url)
-            if result: self.connections += result
-
 
 
 if __name__ == "__main__":
     base_url = 'https://enigaseluce.com'
-    base_url = 'https://www.groupm.com'
+    #base_url = 'https://www.groupm.com'
+    #base_url = 'http://www.ircouncil.it/'
     wpr = WPR(base_url)
-    wpr.navigate_website('/')
+    wpr.navigate_website('/', 4)
 
-    with open(wpr.domain + '.csv', 'w+') as f:
-        writer = csv.DictWriter(f, fieldnames=wpr.connections[0].keys())
-        writer.writeheader()
-        [writer.writerow(x) for x in wpr.connections]
-    print 'saved '+ wpr.domain + 'out.csv'
-
-
-
-
+    pd.DataFrame(wpr.connections).to_csv(wpr.domain + '.csv', encoding='utf-8')
 
 
 
 
 
 """
-
-
-
-
     soup = get_soup(base_url)
     target_url = soup.findAll('a')
     seed_targets = [get_internal_link(x, base_url) for x in target_url if get_internal_link(x, base_url)]
